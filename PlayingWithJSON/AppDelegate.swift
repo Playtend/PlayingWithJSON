5d2665818a6693f29d6d274acf4353f7d1f4813a
Christopher Taylor <crgt5252@gmail.com>
2018-03-06 19:55:27 -0800

    Creates repository

---
+++ PlayingWithJSON/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PlayingWithJSON
+//
+//  Created by Christopher Taylor on 3/6/18.
+//  Copyright © 2018 Christopher Taylor. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ PlayingWithJSON/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PlayingWithJSON
+//
+//  Created by Christopher Taylor on 3/6/18.
+//  Copyright © 2018 Christopher Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+    }
+}
+
